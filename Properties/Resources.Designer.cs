509369acd48c22985a1ac3d41823118f430b2d6e
woiq <beheerderjoshua@gmail.com>
2023-05-22 16:10:33 +0200

    Little correction. "Looping disabled" image resource wasnt added, resulting in an error. Added this :)

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -113,29 +113,9 @@ namespace Purpleshell.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap OpenFolder1 {
-            get {
-                object obj = ResourceManager.GetObject("OpenFolder1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Refresh {
             get {
                 object obj = ResourceManager.GetObject("Refresh", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Rename {
-            get {
-                object obj = ResourceManager.GetObject("Rename", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
